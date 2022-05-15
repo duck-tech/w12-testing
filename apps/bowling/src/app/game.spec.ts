bd2b76e80825efae17fb1692210fdc2631d8eb96
tech-kelly <78743080+duck-tech@users.noreply.github.com>
2022-05-15 16:40:58 +0800

    第二個紅燈

--- apps/bowling/src/app/game.spec.ts
+++ apps/bowling/src/app/game.spec.ts
@@ -14,5 +14,13 @@ test("gutter game", () => {
   }
   expect(game.score).toBe(0);
 });
+
+test("all one", () => {
+  const game = new Game();
+  for (let i = 0; i < 20; i++) {
+    game.roll(1);
+  }
+  expect(game.score).toBe(20);
+});
 });
 
